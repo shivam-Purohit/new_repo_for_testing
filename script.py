72dd7da0566ed8b58e5e1ab7d893eee8309be3b4
Shivam Purohit <shivampurohit900@gmail.com>
2023-09-09 17:40:02 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 import tweepy
 
 client = tweepy.Client(
-    consumer_key="{{ inputs.api_key }}",
-    consumer_secret="{{ inputs.api_key_secret }}",
-    access_token="{{ inputs.access_token }}",
-    access_token_secret="{{ inputs.access_token_secret }}"
+    consumer_key=os.environ.get("api_key"),
+    consumer_secret=os.environ.get("api_key_secret"),
+    access_token=os.environ.get("access_token"),
+    access_token_secret=os.environ.get("access_token_secret")
 )
-print(f"{input.api_key}")
+print(os.environ.get("api_key"))
 tweet_content = "{{ inputs.status }}"
 # client.create_tweet(text = tweet_content)
