2afe0aff153761a8eeec5ba4e2a771b1bec82de8
Shivam Purohit <shivampurohit900@gmail.com>
2023-09-20 11:22:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,24 +2,20 @@ import tweepy
 import os
 import sys
 
-    api_key = sys.argv[1]
-    api_key_secret = sys.argv[2]
-    access_token = sys.argv[3]
-    access_token_secret = sys.argv[4]
-    status = sys.argv[5]
-# api_key_secret, access_token, access_token_secret, status = sys.argv[1:]
+api_key = sys.argv[1]
+api_key_secret = sys.argv[2]
+access_token = sys.argv[3]
+access_token_secret = sys.argv[4]
+status = sys.argv[5]
 
-    client = tweepy.Client(
+client = tweepy.Client(
         consumer_key=api_key,
         consumer_secret=api_key_secret,
         access_token=access_token,
         access_token_secret=access_token_secret
-    )
-    print("is this printing")
-    print(api_key)  # Print the API key to verify it's received correctly
+)
+print("is this printing")
+print(api_key)  # Print the API key to verify it's received correctly
 
-    tweet_content = status  # Use the 'status' input variable
-    client.create_tweet(text = tweet_content)
-
-    # Now you can use the 'tweet_content' and 'client' objects as needed in your script.
-
+tweet_content = status  # Use the 'status' input variable
+client.create_tweet(text = tweet_content)
