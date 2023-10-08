7b3d448d54df25cf807b44d772402bd811d29eb0
shivam purohit <shivampurohit900@gmail.com>
2023-10-08 17:01:56 +0530

    node support

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { TwitterClient } = require('twitter-api-client');
+const { TwitterApi  } = require('twitter-api-v2');
 
 const {
   INPUT_STATUS,
@@ -8,21 +8,23 @@ const {
   INPUT_ACCESS_TOKEN_SECRET,
 } = process.env;
 
-
 (async () => {
+
     try {
-        const twitterClient = new TwitterClient({
-            apiKey: INPUT_API_KEY,
-            apiSecret: INPUT_API_KEY_SECRET,
-            accessToken: INPUT_ACCESS_TOKEN,
-            accessTokenSecret: INPUT_ACCESS_TOKEN_SECRET,
+        const userClient = new TwitterApi({
+          appKey: INPUT_API_KEY,
+          appSecret: INPUT_API_KEY_SECRET,
+          accessToken:  INPUT_ACCESS_TOKEN,
+          accessSecret: INPUT_ACCESS_TOKEN_SECRET,
         });
-        const rwClient = twitterClient.readWrite;
-      await rwClient.v2.tweet(INPUT_STATUS);
+
+      await userClient.v2.tweet(INPUT_STATUS);
       process.exit(1);
+
     } catch (err) {
       console.log(err);
       console.log(`::error ::${err.data}`);
       process.exit(1);
     }
+
   })();
